2043126552ef65f1d34587f7f49d4049e07365a9
Qazaidi123 <zaidi.qumar@gmail.com>
2026-02-13 22:33:14 +0530

    edit app.py file

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 from fastapi import FastAPI
 myapp=FastAPI()
 @myapp.get('/")
+def homepage():
+    return {
+        "School":"Netligent IT Solutions",
+        "Location":"Bhopal",
+        "Message":"We are commited to build future"
+    }
+@myapp.get("/about")
+def about_institute():
+    return {
+        "about":"Netligent IT solutions provide the quality and most demanding courses in IT industry"
+    }
+@myapp.get("/Courses")
+def courses():
+    return {
+        "courses":[
+            "Python",
+            "Devops",
+            "Network Architecture",
+            "Cyber Secuiriy",
+            "Data Analytics"
+        ]
+    }    
+@myapp.get("/contac")
+def contact():
+    return {
+        "address":"Building 4, 2nd Floor, Krishak nagar, karond, Bhopal"
+    }    
            
            
 
